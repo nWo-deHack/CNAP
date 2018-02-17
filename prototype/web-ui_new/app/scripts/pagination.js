632437890eb60db40262a7affd575e7928c38c2b
benkevych <bohdan.benkevych@gmail.com>
2018-02-17 09:43:32 +0200

    new web ui added

---
+++ prototype/web-ui_new/app/scripts/pagination.js
@@ -0,0 +1,3 @@
+// $(function () {     $('#user_table').paging({'limit': 10}); });
+// $('#btnApply').click(function () {     var lmt = $('#pglmt').val();
+// $('#user_table').hpaging('newLimit', lmt); });
---
+++ prototype/web-ui_new/app/scripts/tablesorter.js
@@ -0,0 +1,4 @@
+$(document)
+    .ready(function () {
+        $('#user_table').tablesorter();
+    });
